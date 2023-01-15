d5ec67bd2a8b380e748d5e49bc73ce27248ce59f
flawlesshappiness <bkk@arabesque.dk>
2023-01-15 03:55:24 +0100

    Fixed CreateEmptyScript not working correctly when something was selected in the project window

--- Editor/CreateEmptyScript.cs
+++ Editor/CreateEmptyScript.cs
@@ -5,7 +5,6 @@ public class CreateEmptyScript
     [MenuItem("Assets/Create/C# Script (Empty)", false, 80)]
     private static void CreateScript()
     {
-        var path_dest = AssetDatabase.GetAssetPath(Selection.activeInstanceID) + "/New MonoBehaviour.cs";
-        ProjectWindowUtil.CreateScriptAssetFromTemplateFile("Assets/UnitySubmodules/Editor/Templates/EmptyScriptTemplate.cs.txt", path_dest);
+        ProjectWindowUtil.CreateScriptAssetFromTemplateFile("Assets/UnitySubmodules/Editor/Templates/EmptyScriptTemplate.cs.txt", "New MonoBehaviour.cs");
     }
 }
