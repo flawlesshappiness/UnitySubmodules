ef952f02055232fb182e84bcabd18775fa7e5eb2
flawlesshappiness <bkk@arabesque.dk>
2023-02-18 16:53:39 +0100

    Added Databases path to EditorPaths

--- Editor/EditorPaths.cs
+++ Editor/EditorPaths.cs
@@ -2,4 +2,7 @@ public static partial class EditorPaths
 {
     public const string SUBMODULE = "Assets/UnitySubmodules";
     public static readonly string SPRITES = $"{SUBMODULE}/Sprites";
+
+    public  const string DATABASES = "Assets/Resources/Databases";
+    public static readonly string RES_DATABASES = $"Databases";
 }
