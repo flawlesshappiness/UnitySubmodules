9112399cad028e6c76c2089a52498ee8fd62720d
flawlesshappiness <bkk@arabesque.dk>
2023-05-19 03:58:18 +0200

    Update CustomMonoBehaviourExtensions.cs

--- Misc/Extensions/CustomMonoBehaviourExtensions.cs
+++ Misc/Extensions/CustomMonoBehaviourExtensions.cs
@@ -3,19 +3,24 @@ using UnityEngine;
 
 public static class CustomMonoBehaviourExtensions
 {
-    public static CustomCoroutine StartCoroutineWithID(this MonoBehaviour mb, IEnumerator enumerator, string id = "")
+    public static CustomCoroutine StartCoroutineWithID(this MonoBehaviour mb, IEnumerator enumerator, string id = "", bool use_instance_id = true)
     {
         if (string.IsNullOrEmpty(id))
         {
             id = System.Guid.NewGuid().ToString();
+            id += use_instance_id ? mb.GetInstanceID().ToString() : "";
         }
 
         return CoroutineController.Instance.Run(enumerator, mb, id);
     }
 
     public static CustomCoroutine StartCoroutineWithID(this MonoBehaviour mb, IEnumerator enumerator, Object id_object) => 
-        StartCoroutineWithID(mb, enumerator, id_object.GetInstanceID().ToString());
+        StartCoroutineWithID(mb, enumerator, id_object.GetInstanceID().ToString(), false);
 
-    public static CustomCoroutine StartCoroutineWithID(this MonoBehaviour mb, IEnumerator enumerator) =>
-        StartCoroutineWithID(mb, enumerator, mb.GetInstanceID().ToString());
+    public static void StopCoroutineWithID(this MonoBehaviour mb, string id, bool use_instance_id = true)
+    {
+        if (string.IsNullOrEmpty(id)) return;
+        id += use_instance_id ? mb.GetInstanceID().ToString() : "";
+        CoroutineController.Instance.Kill(id);
+    }
 }
