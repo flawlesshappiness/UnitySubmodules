dfce2a56e33d29e2946f4f6ab43709e5b27d5e20
flawlesshappiness <bkk@arabesque.dk>
2024-02-09 16:46:13 +0100

    Updated log controller and added feature to write file to persistent data

--- Misc/LogController.cs
+++ Misc/LogController.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using UnityEngine;
 
@@ -12,38 +15,116 @@ public class LogController : Singleton
 
     private Color c_exception = new Color(0.9f, 0.3f, 0.3f);
 
+    private const string LOG_FILENAME = "log.txt";
+
     public class Message
     {
-        public float time;
+        public string timeUtc;
+        public string timeGame;
         public string message;
+        public Color color = Color.white;
 
-        public Message(float time, string message)
+        public Message(string message)
         {
-            this.time = time;
             this.message = message;
         }
+
+        public string GetDebugMessage() => $"[{timeGame}] {message.Color(color)}";
+
+        public string GetLogMessage() => $"[{timeUtc}] {message}";
     }
 
     protected override void Initialize()
     {
         base.Initialize();
-
         Application.logMessageReceived += LogException;
+
+        LogMessage("LogController initialized");
+    }
+
+    private void OnApplicationQuit()
+    {
+        WriteToPersistentData();
+    }
+
+    private void WriteToPersistentData()
+    {
+        try
+        {
+            string filePath = Path.Combine(Application.persistentDataPath, LOG_FILENAME);
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                foreach (var m in logged_messages)
+                {
+                    try
+                    {
+                        var message = m.GetLogMessage();
+                        writer.WriteLine(message);
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.Log("Failed to write log file to persistent data");
+        }
+    }
+
+    public void LogException(Exception e)
+    {
+        try
+        {
+            LogMessage(e.Message).color = c_exception;
+            LogMessage(e.StackTrace);
+        }
+        catch (Exception ex)
+        {
+            Debug.Log("Failed to log exception: " + ex.Message);
+        }
     }
 
     private void LogException(string condition, string stacktrace, LogType type)
     {
-        if(type == LogType.Exception)
+        try
         {
-            var m = new Message(Time.time, stacktrace);
-            LogMessage($"{condition.Color(c_exception)}\n{stacktrace}");
+            if (type == LogType.Exception)
+            {
+                LogMessage($"{condition}").color = c_exception;
+                LogMessage($"{stacktrace}");
+            }
         }
+        catch (Exception ex)
+        {
+            LogMessage("Failed to log exception properly. Trying again:");
+            LogMessage(condition);
+            LogMessage(stacktrace);
+        }
+
+        WriteToPersistentData();
     }
 
-    public void LogMessage(string message)
+    public Message LogMessage(string message)
     {
-        var m = new Message(Time.time, message);
-        logged_messages.Add(m);
-        Debug.Log($"{message}");
+        try
+        {
+            var m = new Message(message)
+            {
+                timeGame = Time.time.ToString(),
+                timeUtc = DateTime.UtcNow.ToString("hh:mm:ss.f", CultureInfo.InvariantCulture)
+            };
+
+            logged_messages.Add(m);
+            return m;
+        }
+        catch (Exception e)
+        {
+            var m = new Message(message);
+            logged_messages.Add(m);
+            return m;
+        }
     }
 }
